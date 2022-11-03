dec451d1947b14eaaa4bd48e843949ac6be35713
mansoor666 <56027612+mansoor666@users.noreply.github.com>
2022-11-03 12:19:20 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./background.svg";
+import NavigationRoutes from "./NavigationRoutes";
+import './App.css'
+
+const App = () => {
+  return (
+  <>
+  <NavigationRoutes />;
+  {/* <div className="App">
+    <img src={logo} height="100" alt="background Logo" />
+
+  </div> */}
+  </>
+  );
+};
+
+export default App;
